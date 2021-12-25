e911151aedd486138ee18990631d1978d6da1234
baralsaugat <baralsaugat@gmail.com>
2021-12-26 04:11:38 +1100

    26 Dec 4:11 am

---
+++ recursion/someRecursive.js
@@ -0,0 +1,19 @@
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+}
+
+// flattening the array with in arrays
+
+function flatten(oldArr) {
+  var newArr = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
